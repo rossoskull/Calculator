ba5caf0d444ae4a85adc40d56d08b91cddbeb808
Jay Mistry <mistry.jay15@gmail.com>
2018-11-16 14:25:35 +0530

    Build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Calculator/precache-manifest.9e9a0f31fe818a7791ba349af7cb6a62.js"
+  "/Calculator/precache-manifest.b6117352485d7f7e47e49113884c5795.js"
 );
 
 workbox.clientsClaim();
