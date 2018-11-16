03c9245cf449dfde5623f1f5f64b9a16f55e3b2b
Jay Mistry <mistry.jay15@gmail.com>
2018-11-16 10:12:12 +0530

    add build command

---
+++ docs/precache-manifest.b52dd114162ff52a248037738cce51b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9548f5398287e4f77228",
+    "url": "/static/js/main.9548f539.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "415b6969824c2969d42696a1702f3971",
+    "url": "/index.html"
+  }
+];
